0b2d39860e42b71d346008f96c6edba2b8e8ff50
Kim Kitae <daearcdo@gmail.com>
2024-10-18 14:52:19 +0900

    take screenshot 추가, 테스트 성공/실패 시 스크린샷 report로 발송 추가

--- conftest.py
+++ conftest.py
@@ -1,9 +1,14 @@
+import base64
+import os
+from io import BytesIO
+from PIL import Image
 import pytest
 import logging
 from reportportal_client import RPLogHandler
 from appium_server import AppiumServer
 from driver_manager import DriverManager
 from helper.execute_method import ExecuteMethod
+from reportportal_client import RPLogger
 
 
 """
@@ -49,24 +54,51 @@ def driver(appium_server, os_type, rp_logger):
     driver_manager.quit_driver()
 
 @pytest.fixture(scope="session")
-def rp_logger():
+def rp_logger(request):
     """
     매 세션 마다 로그 서러정 및 reportPortal 로그 핸들 설정
     """
     # 기본 로거 설정
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+    logging.setLoggerClass(RPLogger)
 
-    # 이미 핸들러가 추가되었는지 확인
-    if not logger.handlers:
-        # ReportPortal 핸들러 추가 (핸들러가 없을 때만 추가)
-        rp_handler = RPLogHandler()
-        rp_handler.setLevel(logging.INFO)
-        logger.addHandler(rp_handler)
-    
     # python-dotenv의 WARNING 로그를 ERROR 이상으로만 출력되도록 설정
     logging.getLogger("dotenv.main").setLevel(logging.ERROR)
     # urllib3의 WARNING 로그를 ERROR 이상으로만 출력되도록 설정
     logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
 
     return logger
+    return logger
+
+
+
+def take_screenshot_and_log(driver, os_type, rp_logger, item):
+    """
+    스크린샷 찍기 및 ReportPortal로 로그와 함께 전송
+    """
+    screenshot_name = f"screenshot_{os_type}_{item.name}.png"
+    screenshot = driver.get_screenshot_as_png()
+
+    rp_logger.info("스크린샷 확인:", attachment={
+        'name': screenshot_name,
+        'data': screenshot,
+        'mime': 'image/png'
+    })
+
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call): 
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed or rep.passed:
+        try:
+            driver = item.funcargs['driver']
+            os_type = item.funcargs['os_type']
+            rp_logger = item.funcargs['rp_logger']
+            take_screenshot_and_log(driver, os_type, rp_logger, item)
+
+        except Exception as e:
+            if "rp_logger" in item.funcargs:
+                item.funcargs["rp_logger"].info(f"Failed to take screenshot: {e}")
